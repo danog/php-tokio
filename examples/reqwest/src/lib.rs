79110a4133c0243af8a07aef7ad515dc1ace6b92
coderswat <abhishek.b4696@gmail.com>
2023-08-29 19:19:29 +0530

    organized imports, formatted code with cargo fmt

--- examples/reqwest/src/lib.rs
+++ examples/reqwest/src/lib.rs
@@ -1,24 +1,19 @@
-use php_tokio::{EventLoop, php_async_impl};
 use nicelocal_ext_php_rs::prelude::*;
+use php_tokio::{php_async_impl, EventLoop};
 
 #[php_class]
-struct Client {
-}
+struct Client {}
 
 #[php_async_impl]
 impl Client {
-    pub fn init() -> PhpResult<u64>{
+    pub fn init() -> PhpResult<u64> {
         EventLoop::init()
     }
     pub fn wakeup() -> PhpResult<()> {
         EventLoop::wakeup()
     }
     pub async fn get(url: &str) -> anyhow::Result<String> {
-        Ok(reqwest::get(url)
-            .await?
-            .text()
-            .await?
-        )
+        Ok(reqwest::get(url).await?.text().await?)
     }
 }
 
@@ -29,7 +24,5 @@ pub extern "C" fn request_shutdown(_type: i32, _module_number: i32) -> i32 {
 
 #[php_module]
 pub fn get_module(module: ModuleBuilder) -> ModuleBuilder {
-    module
-        .request_shutdown_function(request_shutdown)
+    module.request_shutdown_function(request_shutdown)
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod borrow_unchecked;
 mod event_loop;
 
-pub use php_tokio_derive::php_async_impl;
-pub use event_loop::RUNTIME;
 pub use event_loop::EventLoop;
+pub use event_loop::RUNTIME;
+pub use php_tokio_derive::php_async_impl;
